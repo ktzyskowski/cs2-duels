91b0eb6d509b3f54c8367744f5bad255282b2d02
ktzyskowski <ktzyskowski@gmail.com>
2024-09-09 10:35:12 -0400

    Remove old code

--- main.py
+++
@@ -1,7 +0,0 @@
-from processing.features.marshall import extract_features_from_rounds
-from processing.parser import parse_json
-
-if __name__ == "__main__":
-    parsed_rounds = list(parse_json(path="res/json/0a5040e7-f972-4e0d-b9bf-ee6dfbdf0342.json"))
-    features = extract_features_from_rounds(parsed_rounds)
-    print(len(parsed_rounds))
--- src/processing/utils.py
+++
@@ -1,9 +0,0 @@
-def sliding_window(lst: list, size: int):
-    """Generate a sliding window over a list.
-
-    :param lst: the data.
-    :param size: the window size.
-    :return: a generator of sliding windows.
-    """
-    for idx in range(len(lst) - size + 1):
-        yield lst[idx: idx + size]
