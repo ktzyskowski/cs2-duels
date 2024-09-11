794339b836d0075b0665851372eecdf739f0aac8
ktzyskowski <ktzyskowski@gmail.com>
2024-09-11 10:42:08 -0400

    Update Side enum

--- src/processing/data/side.py
+++ src/processing/data/side.py
@@ -4,7 +4,11 @@ from enum import Enum
 class Side(Enum):
     """Represents the side (team) of the player.
 
-    Can either be ``Side.ATTACKER`` or ``Side.DEFENDER``. Sides swap halfway through the game, after 12 rounds.
+    Can either be ``Side.ATTACKER`` or ``Side.DEFENDER``. Sides swap halfway through the game, after 12 rounds. It is
+    important to distinguish between attacker and defender when predicting duel outcomes, rather than treating either
+    player as "player1" and "player2", because the rules of Counter Strike influence the decision-making of either
+    party. For example, the attackers must push for control of the map in order to accomplish their objectives, leading
+    to more aggressive plays for attackers than for defenders, who conversely must defend their territory.
     """
 
     ATTACKER = "attacker"
@@ -14,5 +18,6 @@ class Side(Enum):
     def from_str(cls, side: str):
         if side == "T":
             return cls.ATTACKER
-        else:
+        elif side == "CT":
             return cls.DEFENDER
+        raise ValueError(f"Unknown side: {side}")
